a9c57690e3229040fe1e152c2f2b9aeca4c88726
Replit user <>
2023-03-28 18:49:16 +0800

    add /json

--- myApp.js
+++ myApp.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.sendFile(absoultePath);
 });
 
+app.get("/json", (req, res) => {
+  res.json({"message": "Hello json"});
+});
+
 app.use('/public', express.static(publicAbsoultePath))
 
 module.exports = app;
