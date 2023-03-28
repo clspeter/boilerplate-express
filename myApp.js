e66693d0b139f0ea3ea4a6d78a62bfc0969c697f
Replit user <>
2023-03-28 22:17:13 +0800

    time server

--- myApp.js
+++ myApp.js
@@ -1,19 +1,37 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
 const absoultePath = __dirname + '/views/index.html';
 const publicAbsoultePath = __dirname + '/public'
 
-console.log(publicAbsoultePath);
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
 app.get("/", (req, res) => {
   res.sendFile(absoultePath);
 });
 
 app.get("/json", (req, res) => {
-  res.json({"message": "Hello json"});
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
 });
 
 app.use('/public', express.static(publicAbsoultePath))
+
+
 
 module.exports = app;
