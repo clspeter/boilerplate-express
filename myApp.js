16a432e1217d56c28e8a1cc336839531a1106e70
Replit user <>
2023-03-28 17:06:11 +0800

    lesson 13

--- myApp.js
+++ myApp.js
@@ -1,39 +1,15 @@
 let express = require('express');
 let app = express();
 
+const absoultePath = __dirname + '/views/index.html';
+const publicAbsoultePath = __dirname + '/public'
 
+console.log(publicAbsoultePath);
 
+app.get("/", (req, res) => {
+  res.sendFile(absoultePath);
+});
 
+app.use('/public', express.static(publicAbsoultePath))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
